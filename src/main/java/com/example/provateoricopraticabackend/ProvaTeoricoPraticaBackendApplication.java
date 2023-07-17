2c4017f65108f12a9bcf9959d2e6a453fe336e86
Daniele Accossato <daniele.akk@gmail.com>
2023-07-17 10:35:37 +0200

    Add base structure

---
+++ src/main/java/com/example/provateoricopraticabackend/ProvaTeoricoPraticaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.provateoricopraticabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaTeoricoPraticaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaTeoricoPraticaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provateoricopraticabackend/ProvaTeoricoPraticaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provateoricopraticabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaTeoricoPraticaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
